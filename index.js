85c15b0645309a555bc2c2b4350527a0500f5a9c
Sherwin Samson <46096486+sherwinsamson1@users.noreply.github.com>
2023-07-17 11:10:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-alert('hello world from github!');
-alert('testing again...');
-alert('updating..');
+alert('hello world from inside this testing folder!');
+const script = window.document.createElement('script');
+const first = window.document.getElementsByTagName('script')[0];
+script.type = 'module';
+script.src = 'http://127.0.0.1:8080/test2.js';
+script.defer = true;
+script.crossOrigin = 'anonymous';
+// Insert our script next to the frist script element
+first.parentNode.insertBefore(script, first);
